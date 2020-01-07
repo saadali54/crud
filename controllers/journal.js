60a4aada8382c66d1263cb2a5ccdf185cde6197d
saadali54 <41243681+saadali54@users.noreply.github.com>
2020-01-07 20:34:55 +0530

    update journal.js

--- controllers/journal.js
+++ controllers/journal.js
@@ -1,36 +1,80 @@
 const express = require('express'),
 router = express.Router(),
+Journal = require('./models/journal')
 
+//Root Route
 router.get('/', (req, res) => {
    res.redirect('/journal')
 })
-   
+
+//Index Route
 router.get('/journal', (req, res) => {
-   res.render('index)
+   Journal.find({},  (err, result)   => {
+      if(err){
+         res.status(404).send(err.message)         
+      }else{
+         res.render('index, {journal:result})
+      }
+   }
 })
 
+//Create Route         
 router.post('/journal', (req, res) => {
-   res.redirect('/journal')
+   Journal.create(req.body.journal,(err,result) => {
+      if(err){
+         res.status(404).send(err.message)         
+      }else{
+         res.redirect('/journal')
+      }
+   }
 })
-   
+
+//New Route   
 router.get('/journal/new', (req, res) => {
    res.render('new')
 })
-   
+
+//Show Route   
 router.get('/journal/:id', (req, res) => {
-   res.render('show')
+   Journal.findById(req.params.id,(err,result) => {
+      if(err){
+         res.status(404).send(err.message)         
+      }else{
+         res.render('show', {journal:result})
+   } 
 })
 
+//Edit Route   
 router.get('/journal/:id/edit', (req, res) => {
-   res.render('edit')
+   Journal.findById(req.params.id,(err,result) => {
+      if(err){
+         res.status(404).send(err.message)     
+      }else{
+         res.render('edit', {journal:result})
+      }
+   }
 })
-   
+ 
+//Update Route   
 router.put('/journal/:id', (req, res) => {
-   res.redirect('/journal' + req.params.id)
+   Journal.findByIdAndUpdate(req.params.id,(err,result) => {
+      if(err){
+         res.status(404).send(err.message)     
+      }else{
+         res.redirect('/journal/'+ req.params.id)
+      }
+   }
 })
   
+//Delete Route   
 router.delete('/journal/:id', (req, res) => {
-   res.redirect('/journal)
+   Journal.findByIdAndDelete(req.params.id,(err,result) => {
+      if(err){
+         res.status(404).send(err.message)     
+      }else{
+         res.redirect('/journal')
+      }
+   }
 })
    
 module.export = router
