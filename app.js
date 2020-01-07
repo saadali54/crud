71433dcd44ae95918400f9a24c7cd6e9c4c7a7fa
saadali54 <41243681+saadali54@users.noreply.github.com>
2020-01-07 22:17:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express'),
 app = express(),
 mongoose = require('mongoose'),
-bodyParser = require('body-parser),
-methodOverride = require('method-override')
-route = require('
+bodyParser = require('body-parser'),
+methodOverride = require('method-override'),
+journalRoute = require('./controllers/journal')
 
 mongoose.connect(process.env.DATABASEURL,{useNewUrlParser:true, useUnifiedTopology:true});
 app.set('view engine','ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
+
+app.use('/',journalRoute)
 
 app.listen(process.env.PORT, process.env.IP);
