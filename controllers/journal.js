0642ad30b1e43800ed2e59b472bf9556406bb0ce
saadali54 <41243681+saadali54@users.noreply.github.com>
2020-01-07 17:34:20 +0530

    update journal.js

--- controllers/journal.js
+++ controllers/journal.js
@@ -1,9 +1,36 @@
 const express = require('express'),
 router = express.Router(),
 
-
-router.get("/", (req, res) => {
-   res.status(200).send('hello world!')
+router.get('/', (req, res) => {
+   res.redirect('/journal')
+})
+   
+router.get('/journal', (req, res) => {
+   res.render('index)
 })
 
+router.post('/journal', (req, res) => {
+   res.redirect('/journal')
+})
+   
+router.get('/journal/new', (req, res) => {
+   res.render('new')
+})
+   
+router.get('/journal/:id', (req, res) => {
+   res.render('show')
+})
+
+router.get('/journal/:id/edit', (req, res) => {
+   res.render('edit')
+})
+   
+router.put('/journal/:id', (req, res) => {
+   res.redirect('/journal' + req.params.id)
+})
+  
+router.delete('/journal/:id', (req, res) => {
+   res.redirect('/journal)
+})
+   
 module.export = router
